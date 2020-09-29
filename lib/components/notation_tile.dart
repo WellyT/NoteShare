d53658ad05d9aede9878321b8767887d08084ae0
Wellyon Thomas <47388425+WellyT@users.noreply.github.com>
2020-09-29 00:20:30 -0300

    Share anotation with other apps: done

--- lib/components/notation_tile.dart
+++ lib/components/notation_tile.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:noteshare/model/Notation.dart';
+import 'package:noteshare/providers/notations.dart';
 import 'package:noteshare/routes/routes.dart';
+import 'package:provider/provider.dart';
+import 'package:share/share.dart';
 
 class NotationTile extends StatelessWidget {
   final Notation notation;
@@ -27,12 +30,38 @@ class NotationTile extends StatelessWidget {
             ),
             IconButton(
               icon: Icon(Icons.share),
-              onPressed: () {},
+              onPressed: () {
+                Share.share(notation.anotation);
+              },
             ),
             IconButton(
               icon: Icon(Icons.delete),
               color: Colors.red,
-              onPressed: () {},
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: Text('Delete notation'),
+                    content: Text('You sure?'),
+                    actions: <Widget>[
+                      FlatButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Cancel'),
+                      ),
+                      FlatButton(
+                        onPressed: () {
+                          Provider.of<Notations>(context, listen: false)
+                              .remove(notation);
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Yes'),
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
           ],
         ),
--- lib/view/user_form.dart
+++ lib/view/user_form.dart
@@ -3,9 +3,31 @@ import 'package:noteshare/model/Notation.dart';
 import 'package:noteshare/providers/notations.dart';
 import 'package:provider/provider.dart';
 
-class UserForm extends StatelessWidget {
+class UserForm extends StatefulWidget {
+  @override
+  _UserFormState createState() => _UserFormState();
+}
+
+class _UserFormState extends State<UserForm> {
   final _form = GlobalKey<FormState>();
+
   final Map<String, String> _formData = {};
+
+  void _loadFormData(Notation notation) {
+    if (notation != null) {
+      _formData['id'] = notation.id;
+      _formData['anotation'] = notation.anotation;
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final Notation notation = ModalRoute.of(context).settings.arguments;
+
+    _loadFormData(notation);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +42,7 @@ class UserForm extends StatelessWidget {
           child: Column(
             children: <Widget>[
               TextFormField(
+                initialValue: _formData['anotation'],
                 decoration: InputDecoration(labelText: 'Insert your notation'),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -33,22 +56,33 @@ class UserForm extends StatelessWidget {
                 },
                 onSaved: (value) => _formData['anotation'] = value,
               ),
-              IconButton(
-                  icon: Icon(Icons.save),
-                  onPressed: () {
-                    final isValid = _form.currentState.validate();
+              Container(
+                color: Colors.green[400],
+                height: 50,
+                width: 100,
+                margin: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.save),
+                        color: Colors.green[50],
+                        onPressed: () {
+                          final isValid = _form.currentState.validate();
 
-                    if (isValid) {
-                      _form.currentState.save();
-                      Provider.of<Notations>(context, listen: false).put(
-                        Notation(
-                          id: _formData['id'],
-                          anotation: _formData['anotation'],
-                        ),
-                      );
-                      Navigator.of(context).pop();
-                    }
-                  })
+                          if (isValid) {
+                            _form.currentState.save();
+                            Provider.of<Notations>(context, listen: false).put(
+                              Notation(
+                                id: _formData['id'],
+                                anotation: _formData['anotation'],
+                              ),
+                            );
+                            Navigator.of(context).pop();
+                          }
+                        }),
+                  ],
+                ),
+              )
             ],
           ),
         ),
